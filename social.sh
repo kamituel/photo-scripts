7ae3545b41446a178205c6f8d2e37f65d77af78b
Kamil Leszczuk <kamituel@gmail.com>
2015-04-02 09:07:54 +0200

    Use font file regardless of from where the script is called

--- social.sh
+++ social.sh
@@ -3,11 +3,13 @@
 # Resizes an image so it longer dimension has 2000px
 # and add a caption.
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 convert $1 \
         -resize 2000x2000\> \
         -bordercolor black \
         -border 2%x \
-        -font UnicaOne-Regular.ttf \
+        -font $DIR/UnicaOne-Regular.ttf \
         -fill '#DDDDDD' \
         -pointsize 30 \
         -gravity north \
