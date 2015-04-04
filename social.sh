034c00615beb9047c9503cf4b9773866a993011b
Kamil Leszczuk <kamituel@gmail.com>
2015-04-04 09:21:56 +0200

    Changed font and border dimensions.

--- social.sh
+++ social.sh
@@ -5,15 +5,20 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+font_color=white
+bg_color=black
+
 convert $1 \
-        -resize 2000x2000\> \
-        -bordercolor black \
-        -border 2%x \
-        -font $DIR/UnicaOne-Regular.ttf \
-        -fill '#EEEEEE' \
-        -pointsize 34 \
+        -resize 2600x2600\> \
+        -background $bg_color \
+        -splice 0x25 \
+        -bordercolor $bg_color \
+        -border 25x25 \
+        -font $DIR/Cabin-Regular-TTF.ttf \
+        -fill $font_color \
+        -pointsize 36 \
         -gravity north \
-        -annotate +0+1 'KAMIL LESZCZUK : KAMITUEL.PL' \
+        -annotate +0+3 'K A M I L   L E S Z C Z U K   -   K A M I T U E L . P L' \
         -append \
         ${1/.jpg/-social.jpg}
 
