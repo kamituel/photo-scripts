582606a8b48eb190b8110ae1f3fc548f9aa4a95f
Kamil Leszczuk <kamituel@gmail.com>
2015-05-13 22:43:24 +0200

    Support for portrait orientation. Better fonts.

--- social.sh
+++ social.sh
@@ -1,24 +1,47 @@
 #!/bin/sh
 
-# Resizes an image so it longer dimension has 2000px
+# Resizes an image so it longer dimension has 2600px
 # and add a caption.
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# Usage:
+# ./social.sh file.jpg [font-size]
+# font-size is optional, use if script makes a bad guess
+# about it.
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# -font $DIR/Cabin-Regular-TTF.ttf \
 font_color=white
 bg_color=black
+
+width=$(identify $1 | cut -d \  -f 3 | cut -d x -f 1)
+height=$(identify $1 | cut -d \  -f 3 | cut -d x -f 2)
+landscape=$(expr $width / $height)
+
+if [ $landscape = 1 ]; then
+	border=$(bc <<< "$width / 100")
+	fontsize=$(bc <<< "$width / 80")
+	text_offset_y=$(bc <<< "$width / 270")
+else
+	border=$(bc <<< "$height / 100")
+	fontsize=$(bc <<< "$height / 100")
+	text_offset_y=$(bc <<< "$height / 200")
+fi
+
+if [ ! -z $2 ]; then
+	fontsize=$2
+fi
 
 convert $1 \
         -resize 2600x2600\> \
         -background $bg_color \
-        -splice 0x25 \
+        -splice 0x${border} \
         -bordercolor $bg_color \
-        -border 25x25 \
-        -font $DIR/Cabin-Regular-TTF.ttf \
+        -border ${border}x${border} \
+        -font $DIR/Inconsolata-Bold.ttf \
         -fill $font_color \
-        -pointsize 36 \
+        -pointsize ${fontsize} \
         -gravity north \
-        -annotate +0+3 'K A M I L   L E S Z C Z U K   -   K A M I T U E L . P L' \
+        -annotate +0+${text_offset_y} 'K A M I L   L E S Z C Z U K   -   K A M I T U E L . P L' \
         -append \
         ${1/.jpg/-social.jpg}
 
