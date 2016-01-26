ee3fb3e31077dcc0b5476bb541f866ddc4523286
Kamil Leszczuk <kamituel@gmail.com>
2016-01-26 19:01:13 +0100

    Thinner border for a montage.

--- montage.sh
+++ montage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+border=2
 
-montage -mode concatenate -tile 2x -geometry 2000x+10+10 -background white "$@"  montage-tmp.jpg
-convert montage-tmp.jpg -bordercolor white -border 10x10 montage-tmp2.jpg
+montage -mode concatenate -tile 2x -geometry 2000x+$border+$border -background white "$@"  montage-tmp.jpg
+convert montage-tmp.jpg -bordercolor white -border $borderx$border montage-tmp2.jpg
 $DIR/social.sh montage-tmp2.jpg
 mv montage-tmp2-social.jpg montage.jpg
 rm montage-tmp.jpg
